f7e7c49da35c8771796ef33ebaaf484630db1cf0
baohong1998 <bao_mu2012@yahoo.com.vn>
2021-03-26 06:16:31 -0400

    f

--- agents/bd3qn/agents/cycle_targetedNode1.py
+++ agents/bd3qn/agents/cycle_targetedNode1.py
@@ -1,7 +1,12 @@
+# Standard library imports
 import os
 import time
 import pdb
+
+# Specialized imports
 import numpy as np
+#import gym
+#import gym_everglades
 
 NODE_CONNECTIONS = {
     1: [2, 4],
@@ -21,7 +26,7 @@ TAR_NODE = {
     # key is target node, entry is next node from index node
     # ex to get to 1 from 11 go to 8  1[(11 - 1)] = 8 
     # subtract 1 for shift in array
-    # also entry is -1 if targetting current node
+    # ALSO entry is -1 if targetting current node
     1: [-1, 1, 4, 1, 2, 3, 4, 5, 7, 7, 8],
     2: [2, -1, 2, 1, 2, 3, 3, 5, 5, 7, 8],
     3: [4, 3, -1, 3, 3, 3, 3, 5, 5, 7, 8],
@@ -37,6 +42,7 @@ TAR_NODE = {
     #TODO Dijkstra's 
 }
 
+
 NUM_GROUPS = 12
 
 ENV_MAP = {
@@ -46,15 +52,18 @@ ENV_MAP = {
     'everglades-vision-stoch': 'EvergladesVisionStochastic-v0',
 }
 
-class Cycle_Target_Node:
+class cycle_targetedNode1:
     def __init__(self, action_space, player_num):
         self.action_space = action_space
         self.num_groups = NUM_GROUPS
+
         self.num_actions = action_space
         self.shape = (self.num_actions, 2)
+
         self.first_turn = True
         self.steps = 0
         self.player_num = player_num
+        #print('player_num: {}'.format(player_num))
 
         # Types:
         #   0 - Controller
@@ -66,63 +75,75 @@ class Cycle_Target_Node:
         self.group_num = 1
         self.node_num = 2
 
-        # determines which node to rush towards
-        self.tarNode = 11
-        # stores group locations
-        self.group_location=[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        #Determines which node to rush towards
+        self.tarNode = 1
+        #stores group locations
+        self.group_location=[1,1,1,1,1,1,1,1,1,1,1,1]
 
-        # TODO could make the script more efficient by checking if each group
-        # can move instead of just trying to move the next seven groups
-        self.group_movable=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        #TODO could make the script more efficient by checking if each group
+        #can move instead of just trying to move the next seven groups
+        self.group_movable=[0,0,0,0,0,0,0,0,0,0,0,0]
+    # end __init__
 
     def get_action(self, obs):
+        #print('!!!!!!! Observation !!!!!!!!')
+        ##print(obs)
+        #print(obs[0])
+        #for i in range(45,101,5):
+        #    print(obs[i:i+5])
+        #print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         action = np.zeros(self.shape)
 
-        # the next line should really be 0, but there is an env bug that the agent doesn't get
+        # The next line should really be 0, but there is an env bug that the agent doesn't get
         # to see the 0th observation to make it's first move; one turn gets blown
         if not self.first_turn:
             self.update_groups(obs)
             action = self.node_rush(action, self.isNodeControlled(obs))
         else:
             self.first_turn = False
+        #print(action)
         
         return action
+    # end get_action
 
-    def act_all_cycle(self,actions = np.zeros((7, 2))):
-        for i in range(0, 7):
+    def act_all_cycle(self,actions=np.zeros((7,2))):
+        for i in range(0,7):
             actions[i] = [self.group_num, self.node_num]
             self.group_num = (self.group_num + 1) % self.grouplen 
             nodetest = (self.node_num) % self.nodelen + 1
             self.node_num = nodetest if self.group_num == 0 else self.node_num
         return actions
+    #end act_all_cycle
 
-    # parameter nodeControl is determined by calling isNodeControlled function
-    def node_rush(self, actions = np.zeros((7, 2)), nodeControl = False):
+    # Parameter nodeControl is determined by calling isNodeControlled function
+    def node_rush(self, actions=np.zeros((7,2)), nodeControl = False):
         if nodeControl:
             return self.act_all_cycle(actions)
         else:
-            # determine next nodes for target
+            #determine next nodes for target
             nextNodes = TAR_NODE[self.tarNode]
-            for i in range(0, 7):
-                # find node to go to for current group number
+            for i in range(0,7):
+                #find node to go to for current group number
                 curNode = int(self.group_location[self.group_num])
                 nextNode = nextNodes[curNode - 1]
 
                 # from original all cycle code
                 actions[i] = [self.group_num, nextNode]
                 self.group_num = (self.group_num + 1) % self.grouplen
-            #print(actions)
+            
             return actions
+    #end node_rush
     
     def update_groups(self, obs):
         i = 45
         index = 0
         while i < 101:
-            #print("index: " + str(index) + ", i: " + str(i))
+            
             self.group_location[index] = obs[i]
-            self.group_movable[index] = obs[i + 3]
+            self.group_movable[index] = obs[i+3]
             i += 5
             index += 1
+    #end get_location
 
     def isNodeControlled(self, obs):
         desiredControlLevel = 75
@@ -130,4 +151,5 @@ class Cycle_Target_Node:
             return True
         else:
             return False
- 
+    #end isNodeControlled
+# end class
--- agents/bd3qn/trainer.py
+++ agents/bd3qn/trainer.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 from torch.utils import tensorboard
-
+import random
 from utils import save_checkpoint, save_best, build_action_table
 from datetime import datetime
 import os
-
+import importlib
 
 class Trainer:
     def __init__(self, model,
@@ -60,8 +60,37 @@ class Trainer:
     def _exploration(self, step):
         return self.epsilon_final + (self.epsilon_start - self.epsilon_final) * np.exp(-1. * step / self.epsilon_decay)
 
+    def _changePlayer(self, player, pid):
+        rand_agent_file = "./{}".format(player)
+        rand_agent_name, rand_agent_extension = os.path.splitext(rand_agent_file)
+        rand_agent_mod = importlib.import_module(
+            rand_agent_name.replace('./', 'agents.'))
+        rand_agent_class = getattr(
+            rand_agent_mod, os.path.basename(rand_agent_name))
+        rand_player = rand_agent_class(self.env.num_actions_per_turn, 0)
+        self.players[pid] = rand_player
+        self.pnames[pid] = rand_player.__class__.__name__
+
     def loop(self):
         #state = self.env.reset()
+        player_list = {
+            'random_actions': 15, 
+            'base_rushV1': 1, 
+            'Cycle_BRush_Turn25': 1, 
+            'Cycle_BRush_Turn50': 1,
+            'Cycle_Target_Node': 1,
+            'cycle_targetedNode1': 1,
+            'cycle_targetedNode11': 1,
+            'cycle_targetedNode11P2': 1,
+            'same_commands': 1,
+            'SwarmAgent': 1
+            }
+        plist = []
+
+        for p in list(player_list.keys()):
+            for i in range(0, player_list[p]):
+                plist.append(p)
+        
         state = self.env.reset(
             players=self.players,
             config_dir=self.config_dir,
@@ -109,19 +138,32 @@ class Trainer:
                         turn_played_by_network += 1
                     else:
                         #print("not here")
-                        action_idx = np.random.choice(
-                            len(self.action_table), size=7)
-                        action[pid] = np.zeros(
-                            (self.env.num_actions_per_turn, 2))
-                        for n in range(0, len(action_idx)):
-                            action[pid][n][0] = self.action_table[action_idx[n]][0]
-                            action[pid][n][1] = self.action_table[action_idx[n]][1]
+                        # action_idx = np.random.choice(
+                        #     len(self.action_table), size=7)
+                        # action[pid] = np.zeros(
+                        #     (self.env.num_actions_per_turn, 2))
+                        # for n in range(0, len(action_idx)):
+                        #     action[pid][n][0] = self.action_table[action_idx[n]][0]
+                        #     action[pid][n][1] = self.action_table[action_idx[n]][1]
+                        agent_id = 0
+                        if pid == 0:
+                            agent_id = 1
+                        
+                        action[pid] = self.players[agent_id].get_action(state[pid])
                 else:
                     action[pid] = self.players[pid].get_action(state[pid])
 
             next_state, reward, done, infos = self.env.step(action)
 
             if done:
+                for pid in self.players:
+                    if pid != self.player_num:
+                        #print(plist)
+                        self.player_name = random.choice(plist)
+                        counter = player_list[self.player_name]
+                        self._changePlayer(self.player_name, pid)
+                        print("Training with {}".format(self.player_name))
+
                 next_state = self.env.reset(
                     players=self.players,
                     config_dir=self.config_dir,
@@ -134,12 +176,12 @@ class Trainer:
                 if reward[self.player_num] == 1:
                     num_of_wins += 1
                 total_games_played += 1
-                print("Result on game {}: {}. Number of moves made by the network: {}/{}".format(
-                    len(all_winrate), reward, turn_played_by_network, total_turn_played))
+                print("Result on game {}: {}. Number of moves made by the network: {}/{}. Agents: {}".format(
+                    len(all_winrate), reward, turn_played_by_network, total_turn_played, self.player_name))
                 episode_winrate = (num_of_wins/total_games_played) * 100
                 all_winrate.append(episode_winrate)
                 with open(os.path.join(path, "rewards-{}.txt".format(time)), 'a') as fout:
-                    fout.write("Winrate: {}. Number of moves made by the network: {}/{}\n".format(episode_winrate, turn_played_by_network, total_turn_played))
+                    fout.write("Winrate: {}. Number of moves made by the network: {}/{}. Agents: {}\n".format(episode_winrate, turn_played_by_network, total_turn_played, self.player_name))
                 print("Current winrate: {}%".format(episode_winrate))
                 w.add_scalar("winrate",
                              episode_winrate, global_step=len(all_winrate))
